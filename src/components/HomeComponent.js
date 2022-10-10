cf5c8f5b2000239ce84eadca737a1c5d4c463561
April Garlington <april.garlington@hotmail.co.uk>
2022-10-10 16:41:31 +0100

    dropdown directs to cinema id page

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,22 +1,40 @@
+import { useNavigate, useParams } from "react-router-dom"
+import { useState } from "react";
+
 const HomeComponent = ({cinemas}) => {
 
-        const cinemaOptions = cinemas.map((cinema) => {
-        return <>
-        <option value={cinema.id}>{cinema.branch}</option>
-        </>
+    const [selectedCinema, setSelectedCinema] = useState(null);
 
-        
+    const navigate = useNavigate();
+
+    const {id} = useParams();
+
+    const cinemaOptions = cinemas.map((cinema) => {
+        return <>
+        <option key={cinema.id} value={cinema.id}>{cinema.branch}</option>
+        </>
     })
+
+    const handleCinemaNavigation = (event) => {
+        event.preventDefault()
+        navigate(`/cinemas/${selectedCinema}`)
+    }
 
     return (
         <>
         <h3>Please select your cinema</h3>
-        <select
+
+        <form onSubmit={handleCinemaNavigation}>
+            <select
             name="cinemas"
+            onChange={(event) => setSelectedCinema(event.target.value)}
             defaultValue="select-cinema">
                 <option disabled-value="select-cinema">Select a cinema</option>
             {cinemaOptions}
             </select>
+            <button type="submit">OK</button>
+        </form>
+        
        
         </>
     )
