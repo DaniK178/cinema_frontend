3c148ede0594596ac56ddacded6832c9f5bb44ed
April Garlington <april.garlington@hotmail.co.uk>
2022-10-13 17:03:02 +0100

    upcoming movies in flex

--- src/components/UpcomingMovies.js
+++ src/components/UpcomingMovies.js
@@ -1,13 +1,16 @@
 const UpcomingMovies = () => {
 
     return (
-        <div>
-            <h2>Upcoming movies!</h2>
+        <>
+        <h2>Upcoming movies!</h2>
+        <br />   
+        <div className="upcomingMovies">   
+      
             <div>
                <img 
                     src="../Photos/Alien.png"
                     alt="Alien frame"
-                    width="30%"
+                    width="80%"
                 />
                 <h3>Alien</h3>
             </div>
@@ -15,7 +18,7 @@ const UpcomingMovies = () => {
                <img 
                 src="../Photos/ToyStory.png"
                 alt="Toy Story frame"
-                width="30%"
+                width="80%"
                 /> 
                 <h3>Toy Story</h3>
             </div>
@@ -23,13 +26,13 @@ const UpcomingMovies = () => {
                 <img 
                 src="../Photos/BlairWitchProject.png"
                 alt="The Blair Witch Project frame"
-                width="30%"
+                width="80%"
                 />
                 <h3>The Blair Witch Project</h3>  
             </div>
         </div>
+        </>
     )
-
 }
 
 export default UpcomingMovies;
