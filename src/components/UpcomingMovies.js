3c1c9d4b4b075795ef80238f3ad960872f9da76a
April Garlington <april.garlington@hotmail.co.uk>
2022-10-13 16:21:24 +0100

    added pictures of upcoming movies

---
+++ src/components/UpcomingMovies.js
@@ -0,0 +1,35 @@
+const UpcomingMovies = () => {
+
+    return (
+        <div>
+            <h2>Upcoming movies!</h2>
+            <div>
+               <img 
+                    src="../Photos/Alien.png"
+                    alt="Alien frame"
+                    width="30%"
+                />
+                <h3>Alien</h3>
+            </div>
+            <div>
+               <img 
+                src="../Photos/ToyStory.png"
+                alt="Toy Story frame"
+                width="30%"
+                /> 
+                <h3>Toy Story</h3>
+            </div>
+            <div>
+                <img 
+                src="../Photos/BlairWitchProject.png"
+                alt="The Blair Witch Project frame"
+                width="30%"
+                />
+                <h3>The Blair Witch Project</h3>  
+            </div>
+        </div>
+    )
+
+}
+
+export default UpcomingMovies;
