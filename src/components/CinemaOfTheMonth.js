7a841c5f24bec3d4fa18c8de17c601a6dbfdbed8
DaniK178 <daniellekyere@gmail.com>
2022-10-14 11:36:45 +0100

    added more text to cinema of the month

--- src/components/CinemaOfTheMonth.js
+++ src/components/CinemaOfTheMonth.js
@@ -15,7 +15,19 @@ const CinemaOfTheMonth = () => {
             We bring to our place a part of that special atmosphere, we invite guests. Festivals are also an opportunity to give prizes and to promote young directors or less known films. During such events we reach new audiences, we talk to our viewers and they can meet people they know from the big screen. That builds up the feeling that they participate in something more important than a regular film screening.</p>
         <h4>What makes your cinema special?</h4>
         <p>we still have the 35mm projector and we run special screenings from time to time. This is the history of cinema and we want to continue to keep that old fashion style. This is the best way to feel the soul and magic of cinema.</p>
+
+        <h4>What are some things we can look forward to at your cinema?</h4>
+        <p>More event cinema!</p>
+        <p> Live performances from world-class theatre, opera, music and dance to exhilarating sports and gaming events.
+            
+            Customers will get to ynjoy the best seat in the house with beautifully clear on-screen pictures and breath-taking surround sound. Every performance becomes an unforgettable, larger-than-life experience. 
+            </p>
+            <p> It will be an exciting experience for employees!</p>
+
+
         </div>
+
+        
 
         {/* <div className ="CotM-images">  */}
 
